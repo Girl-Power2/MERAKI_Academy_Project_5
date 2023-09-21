84c2b1133d38d06fc51ea299c1a3f6e0345b7bfb
HalaAbuShalbak <halaj.abushalbak@gmail.com>
2023-09-22 01:05:59 +0300

    role_permissions_controller+router

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("./models/db");
 
 //routers
-
+const role_permissionsRouter=require("./routes/role_permissions")
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 // router middleware
-
+app.use("/role_permissions",role_permissionsRouter)
 
 const PORT = process.env.PORT || 5000;
 
