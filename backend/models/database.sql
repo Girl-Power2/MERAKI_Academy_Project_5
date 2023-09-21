aa292611c80a3bf584a2a39a0d4ac07876f55ded
HalaAbuShalbak <halaj.abushalbak@gmail.com>
2023-09-22 00:32:28 +0300

    starter-code-v07

--- backend/models/database.sql
+++ backend/models/database.sql
@@ -14,8 +14,8 @@ PRIMARY KEY (permission_id)
 role_permission_id SERIAL NOT NULL,
 role_id INT,
 permission_id INT,
-FOREIGN KEY (role_id) REFERENCES roles(id),
-FOREIGN KEY (permission_id) REFERENCES permissions(id),
+FOREIGN KEY (role_id) REFERENCES roles(role_id),
+FOREIGN KEY (permission_id) REFERENCES permissions(permission_id),
 PRIMARY KEY (role_permission_id)
 );
 
@@ -49,18 +49,18 @@ category_id INT NOT NULL ,
 role_id INT ,
 FOREIGN KEY (role_id) REFERENCES roles(role_id),
 FOREIGN KEY (category_id) REFERENCES categories(category_id),
-is_deleted DEFAULT 0
-)
+is_deleted SMALLINT DEFAULT 0
+);
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 CREATE TABLE services(
 
 service_id SERIAL PRIMARY KEY NOT NULL ,
 service VARCHAR(1000) NOT NULL ,
-price-per-hour VARCHAR(255),
-provider_id INT
+price_per_hour VARCHAR(255),
+provider_id INT,
 FOREIGN KEY (provider_id) REFERENCES providers(provider_id),
 
-is_deleted DEFAULT 0
+is_deleted SMALLINT DEFAULT 0
 );
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 CREATE TABLE orders(
@@ -72,18 +72,18 @@ FOREIGN KEY (provider_id) REFERENCES providers(provider_id),
 user_id INT NOT NULL ,
 FOREIGN KEY (user_id) REFERENCES users(user_id),
 created_at TIMESTAMP DEFAULT NOW(),
-is_deleted DEFAULT 0,
-status DEFAULT "pending",
+is_deleted  SMALLINT DEFAULT 0,
+status VARCHAR DEFAULT 'pending',
 schedule_id INT,
-FOREIGN KEY (schedule_id) REFERENCES schedules(schedule_id),
+FOREIGN KEY (schedule_id) REFERENCES schedules(schedule_id)
 );
 >>>>>>>>>>>>>>>>>>>>>>>>>>>
 CREATE TABLE provider_info(
 provider_id SERIAL PRIMARY KEY NOT NULL,
 img TEXT,
-bio MEDIUMTEXT NOT NULL,
-qualifications MEDIUMTEXT NOT NULL,
-is_deleted DEFAULT 0
+bio TEXT NOT NULL,
+qualifications TEXT NOT NULL,
+is_deleted SMALLINT DEFAULT 0
 );
 -------------------
 CREATE TABLE provider_notes(
@@ -93,25 +93,24 @@ FOREIGN KEY (user_id) REFERENCES users(user_id),
 provider_id INT,
 FOREIGN KEY (provider_id) REFERENCES providers(provider_id),
 visitied_on TIMESTAMP DEFAULT NOW (),
-note MEDIUMTEXT NOT NULL,
-is_deleted DEFAULT 0
+note TEXT NOT NULL,
+is_deleted SMALLINT DEFAULT 0
 );
 >>>>>>>>>>>>>>>>>>>>>>>>>>>
 CREATE TABLE medical_history(
 medical_history_id SERIAL PRIMARY KEY NOT NULL,
-user_id INT
+user_id INT,
 FOREIGN KEY (user_id) REFERENCES users(user_id),
-history MEDIUMTEXT,
-medications MEDIUMTEXT,
-chronic_diseases  MEDIUMTEXT,
-,
-is_deleted DEFAULT 0
+history TEXT,
+medications TEXT,
+chronic_diseases  TEXT,
+is_deleted SMALLINT DEFAULT 0
 );
 >>>>>>>>>>>>>>>>>>>>>>>>>>>
 CREATE TABLE categories(
 category_id SERIAL PRIMARY KEY NOT NULL ,
 category VARCHAR(255),
-is_deleted DEFAULT 0
+is_deleted SMALLINT DEFAULT 0
 );
 >>>>>>>>>>>>>>>>>>>>>>>>>>>
 CREATE TABLE schedules(
@@ -119,8 +118,8 @@ schedule_id SERIAL PRIMARY KEY NOT NULL ,
 date DATE ,
 provider_id INT,
 FOREIGN KEY (provider_id) REFERENCES providers(provider_id),
-is_deleted DEFAULT 0,
+is_deleted SMALLINT DEFAULT 0,
 user_id INT,
 FOREIGN KEY (user_id) REFERENCES users(user_id),
-booked DEFAULT false
+booked BOOLEAN DEFAULT false
 )
