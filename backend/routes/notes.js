e4fe4fc30c358989b8435257272500decc91e6a4
HalaAbuShalbak <halaj.abushalbak@gmail.com>
2023-09-25 00:11:51 +0300

    notes

---
+++ backend/routes/notes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { notes } = require("../controllers/notes");
+const authentication = require("../middlewares/authentication");
+const authorization = require("../middlewares/authorization");
+
+const notesRouter = express.Router();
+notesRouter.post("/", authentication, notes.AddNote);
+notesRouter.get("/byProvider/:id", authentication, notes.GetNotebyProviderId);
+notesRouter.delete("/:id", authentication, notes.DeleteNote);
+notesRouter.put("/:id", authentication, notes.UpdateNote);
+
+module.exports = notesRouter;
